fce92b85919fa3128cf7c9365a4248de790fc9d5
Tarun Tehri <tehritarun@gmail.com>
2026-08-21 21:59:26 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module wordle_cli
+
+go 1.26.5
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
+func main() {
+	fmt.Println(validateWord("RETRO", "ETROR"))
+	fmt.Println(validateWord("RETRO", "MATRO"))
+	fmt.Println(validateWord("RETRO", "RTROE"))
+	fmt.Println(validateWord("RETRO", "RETRE"))
+}
+
+func validateWord(answer string, testWord string) string {
+	answerArr := strings.Split(strings.ToUpper(answer), "")
+	testWordArr := strings.Split(strings.ToUpper(testWord), "")
+
+	correct := "🟩"
+	incorrect := "⬛️"
+	wrongSpot := "🟨"
+
+	result := slices.Repeat([]string{incorrect}, 5)
+
+	// first iteration for correct
+	for i, c := range testWordArr {
+		if answerArr[i] == c {
+			result[i] = correct
+			answerArr[i] = "0"
+		}
+	}
+
+	for i, c := range testWordArr {
+		if slices.Contains(answerArr, c) {
+			result[i] = wrongSpot
+			idx := slices.Index(answerArr, c)
+			answerArr[idx] = "0"
+		}
+	}
+
+	return strings.Join(result, "")
+}
